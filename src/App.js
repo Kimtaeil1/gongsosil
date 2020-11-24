0c817a8f4c6bc40e28928274377b0b63335186a6
kimtaeil <taeil@Taeilui-MacBookPro.local>
2020-11-24 10:51:51 +0900

    modal issue

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  ChakraProvider,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Button,
+  VStack,
+  useDisclosure,
+  RadioGroup,
+  Radio,
+  Stack,
+} from "@chakra-ui/react";
+import customTheme from "./theme";
 
-function App() {
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react"
+
+var Lorem = require('react-lorem-component');
+
+export default function App() {
+  const { isOpen, onOpen, onClose} = useDisclosure()
+  const [size, setSize] = React.useState("md")
+
+  const [scrollBehavior, setScrollBehavior]=React.useState("inside")
+  const btnRef = React.useRef()
+
+  const sizes = ["xs", "sm", "md", "lg", "xl", "full"]
+
+  const handleSizeClick = (newSize) => {
+    setSize(newSize)
+    onOpen()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider theme={customTheme}>
+      <VStack>
+        <Menu placement="bottom">
+          <MenuButton as={Button}>Full-screen menu toggle</MenuButton>
+          <MenuList>
+            <MenuItem>Download</MenuItem>
+            <MenuItem>Create a Copy</MenuItem>
+            <MenuItem>Mark as Draft</MenuItem>
+            <MenuItem>Delete</MenuItem>
+            <MenuItem>Attend a Workshop</MenuItem>
+          </MenuList>
+        </Menu>
+      </VStack>
+      
+      <RadioGroup value={scrollBehavior} onChange={setScrollBehavior}>
+        <Stack direction="row">
+          <Radio value="inside">inside</Radio>
+          <Radio value="outside">outside</Radio>
+        </Stack>
+      </RadioGroup>
+
+      {sizes.map((size)=>
+        <Button
+          onClick={()=>handleSizeClick(size)}
+          key={size}
+          m={4}
+          mt={3}
+          ref={btnRef}
+          >{`Open ${size}Modal`}</Button>)}
+
+      <Modal
+        onClose={onClose}
+        finalFocusRef={btnRef}
+        isOpen={isOpen}
+        scrollBehavior={scrollBehavior}
+        size={size}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Lorem count={20} />
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>Close</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </ChakraProvider>
+
+    
   );
 }
-
-export default App;
